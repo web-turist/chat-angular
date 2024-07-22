7122e752a35134a113dea73b3795502fa76074d8
Maksim Khorkhordin <x.maxim84@mail.ru>
2024-07-22 12:51:33 +0300

    Сделал загрузку данных и вывел карточки профиля

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProfileCardComponent } from './common-ui/profile-card/profile-card.component';
+import { Profile } from './data/interfaces/profile.interface';
+import { ProfileService } from './data/services/profile.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [RouterOutlet, ProfileCardComponent, JsonPipe],
 })
 export class AppComponent {
   title = 'small-talk';
+  profileService = inject(ProfileService);
+  profiles: Profile[] = [];
+
+  constructor() {
+    this.profileService.getAccounts().subscribe((data) => {
+      this.profiles = data;
+    });
+  }
 }
---
+++ src/app/data/services/profile.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Profile } from '../interfaces/profile.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProfileService {
+  http = inject(HttpClient);
+  baseApiUrl = 'https://icherniakov.ru/yt-course/';
+
+  constructor() {}
+
+  getAccounts() {
+    return this.http.get<Profile[]>(`${this.baseApiUrl}account/test_accounts`);
+  }
+}
