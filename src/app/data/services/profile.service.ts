e8b385d770767b996ca4c577d7d6774d823d0541
Maksim Khorkhordin <x.maxim84@mail.ru>
2024-07-30 18:27:01 +0300

    Настроил получение данных с бэкенда и вывел их

--- src/app/data/services/profile.service.ts
+++ src/app/data/services/profile.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable, inject } from '@angular/core';
-import { Profile } from '../interfaces/profile.interface';
+import { inject, Injectable, signal } from '@angular/core';
+import { map, tap } from 'rxjs';
+import { Pageble } from './../interfaces/pageble.interface';
+import { Profile } from './../interfaces/profile.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -8,8 +10,9 @@ import { Profile } from '../interfaces/profile.interface';
 export class ProfileService {
   http = inject(HttpClient);
   baseApiUrl = 'https://icherniakov.ru/yt-course/';
-
-  constructor() {}
+  // me!: Profile
+  me = signal<Profile | null>(null);
+  // constructor() {}
 
   getAccounts() {
     return this.http.get<Profile[]>(`${this.baseApiUrl}account/test_accounts`);
@@ -17,5 +20,17 @@ export class ProfileService {
 
   getMe() {
     return this.http.get<Profile>(`${this.baseApiUrl}account/me`)
+      .pipe(
+        tap((res: Profile) => this.me.set(res))
+      )
+  }
+
+  getSubscribersShortList() {
+    return this.http.get<Pageble<Profile>>(`${this.baseApiUrl}account/subscribers/`)
+      .pipe(
+        map((res: Pageble<Profile>): Profile[] => {
+          return res.items.slice(0, 4);
+        })
+      )
   }
 }
